063d6874f6d923d82407840ecaf58a267ea1afea
849728 <63786225+cptyagi007@users.noreply.github.com>
2020-04-26 09:56:05 +0530

    Add files via upload

---
+++ OrmLearnApplication.java
@@ -0,0 +1,48 @@
+package com.cognizant.ormlearn;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	
+	private static CountryService countryService;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+	public static void main(String[] args) {
+		
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
+
+		 testGetAllCountries();
+		LOGGER.info("Inside main");
+	}
+	private static void testGetAllCountries() {
+
+		LOGGER.info("Start");
+
+		List<Country> countries = new ArrayList<Country>();
+				countries=countryService.getAllCountries();
+				for(Country c:countries) {
+					System.out.println(c.getName());
+				}
+		
+
+		LOGGER.debug("countries={}", countries);
+
+		LOGGER.info("End");
+
+		}
+
+}
